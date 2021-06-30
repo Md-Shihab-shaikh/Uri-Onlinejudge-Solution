014cf799599265da064be3986f86141fb59d351b
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-06-30 23:40:31 +0600

    Simple Sum

---
+++ 1003.java
@@ -0,0 +1,18 @@
+package uri;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a,b,c;
+		 Scanner sc=new Scanner(System.in);
+		 a=sc.nextInt();
+		 b=sc.nextInt();
+		 c=a+b;
+	        System.out.printf("SOMA = "+c);
+
+	}
+
+}
