d333f1b50cb2294b73fc4db5bb9b660ab880ea18
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-06-30 22:44:49 +0600

    Area of a Circle

---
+++ 1002.java
@@ -0,0 +1,17 @@
+package uri;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  double R,A;
+	        Scanner sc = new Scanner(System.in);
+	        R = sc.nextDouble();
+	        A = 3.14159 * R * R;
+	        System.out.printf("A=%.4f\n", A);
+
+	}
+
+}
