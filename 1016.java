bd47ba954b9ac11494de6a5657be969871307f03
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-11-04 19:46:44 +0600

    Distance

---
+++ 1016.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ Scanner Sc=new Scanner(System.in);
+			 int t = Sc.nextInt();
+		int k = t*2;
+		System.out.println(k+" minutos");
+ 
+    }
+}
