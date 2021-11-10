7b07fb914cb006e10dcc41be5d62cbc5a619b61e
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-11-10 23:35:06 +0600

    Month

---
+++ 1052.java
@@ -0,0 +1,39 @@
+ import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int b;
+Scanner sc =new Scanner(System.in);
+
+b = sc.nextInt();
+
+if(b==1)
+	 System.out.printf("January\n");
+else if(b==2)
+	System.out.printf("February\n");
+else if(b==3)
+	System.out.printf("March\n");
+else if(b==4)
+	System.out.printf("April\n");
+else if(b==5)
+	System.out.printf("May\n");
+else if(b==6)
+	System.out.printf("June\n");
+else if(b==7)
+	System.out.printf("July\n");
+else if(b==8)
+	System.out.printf("August\n");
+else if(b==9)
+	System.out.printf("September\n");
+else if(b==10)
+	System.out.printf("October\n");
+else if(b==11)
+	System.out.printf("November\n");
+else if(b==12)
+	System.out.printf("December\n");
+
+}
+}
