443aea2ed0e43cf4668bb4ee06704b7d5ad22b41
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-11-30 13:11:07 +0600

    Remaining 2

---
+++ 1075.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i,n;
+		Scanner Sc =new Scanner(System.in);
+		n=Sc.nextInt();
+	    for(i=2;i<=10000;i=i+n)
+	    {
+	        System.out.println(i);
+	    }		
+		
+		
+	}
+
+}
