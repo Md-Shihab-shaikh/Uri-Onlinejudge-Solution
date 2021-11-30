87d3baebf4d63782b75b73fe15fa535563584a8a
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-11-30 11:27:59 +0600

    Even Square

---
+++ 1073.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int p;
+		Scanner Sc =new Scanner(System.in);
+		p=Sc.nextInt();
+		
+		for(int i=1; i<=p; i++) {
+			
+      if(i%2==0)
+      {
+    	  System.out.println(i+"^2 = "+(i*i));
+      }
+				
+}
+}
+}
