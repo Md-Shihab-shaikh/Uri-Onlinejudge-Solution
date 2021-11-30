ef83573db44791a5286b536c45670d9c84135c49
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2021-11-30 13:25:15 +0600

    import java.util.Scanner; public class Main { 	public static void main(String[] args) { 		// TODO Auto-generated method stub  		int n; 		int a,b,c,d,e,f,g,h,i,j; 		Scanner Sc =new Scanner(System.in); 		n=Sc.nextInt(); 	   		a=1*n; 		b=2*n; 		c=3*n; 		d=4*n; 		e=5*n; 		f=6*n; 		g=7*n; 		h=8*n; 		i=9*n; 		j=10*n; 		System.out.println("1 x "+n+" = "+a); 		System.out.println("2 x "+n+" = "+b); 		System.out.println("3 x "+n+" = "+c); 		System.out.println("4 x "+n+" = "+d); 		System.out.println("5 x "+n+" = "+e); 		System.out.println("6 x "+n+" = "+f); 		System.out.println("7 x "+n+" = "+g); 		System.out.println("8 x "+n+" = "+h); 		System.out.println("9 x "+n+" = "+i); 		System.out.println("10 x "+n+" = "+j); 		 		 	}  }

---
+++ 1078.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n;
+		int a,b,c,d,e,f,g,h,i,j;
+		Scanner Sc =new Scanner(System.in);
+		n=Sc.nextInt();
+	  
+		a=1*n;
+		b=2*n;
+		c=3*n;
+		d=4*n;
+		e=5*n;
+		f=6*n;
+		g=7*n;
+		h=8*n;
+		i=9*n;
+		j=10*n;
+		System.out.println("1 x "+n+" = "+a);
+		System.out.println("2 x "+n+" = "+b);
+		System.out.println("3 x "+n+" = "+c);
+		System.out.println("4 x "+n+" = "+d);
+		System.out.println("5 x "+n+" = "+e);
+		System.out.println("6 x "+n+" = "+f);
+		System.out.println("7 x "+n+" = "+g);
+		System.out.println("8 x "+n+" = "+h);
+		System.out.println("9 x "+n+" = "+i);
+		System.out.println("10 x "+n+" = "+j);
+		
+		
+	}
+
+}
