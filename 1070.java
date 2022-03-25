f7bd838f8e77d57816e0b48cb341acf22773c000
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2022-03-25 19:56:04 +0600

    Six Odd Numbers

--- 1070.java
+++ 1070.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x,count =6;;
+		   Scanner sc =new Scanner(System.in);
+		   x = sc.nextInt();
+
+		   for(int i=x; i< (x+(count*2)); i++)
+		   {
+			   if(i%2 != 0) {
+			 System.out.println(i);
+			  
+		   }
+		   }
+		   
+		   
+		   
+		   
+		   
+			}
+		   
+}
