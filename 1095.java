9e7a3901a845f3264a7682056859f013d13fdbfd
Md-Shihab-shaikh <85719398+Md-Shihab-shaikh@users.noreply.github.com>
2022-03-25 20:01:05 +0600

    Sequence IJ 1

---
+++ 1095.java
@@ -0,0 +1,22 @@
+package genaral;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i=1;	
+		for(int j=60; j>=0; j-=5) {
+			
+			System.out.println("I="+i+" J="+j);
+			
+	             	i+=3;
+	             	
+		}
+		
+	}
+
+}
+		   
+		     
